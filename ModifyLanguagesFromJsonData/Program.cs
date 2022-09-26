6350b7f514e1707250c6acf51d8aa5705fb8bea1
Nodirkhan <abdumurodovnodirxon@gmail.com>
2022-09-26 08:04:15 +0500

    Delete unnesessary method

--- ModifyLanguagesFromJsonData/Program.cs
+++ ModifyLanguagesFromJsonData/Program.cs
@@ -124,50 +124,5 @@ namespace ModifyLanguagesFromJsonData
                 }
             }
         }
-        public static string TranslateText(string input)
-        {
-            // Set the language from/to in the url (or pass it into this function)
-            string url = String.Format
-            ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
-             "en", "uz", Uri.EscapeUriString(input));
-            HttpClient httpClient = new HttpClient();
-            string result = httpClient.GetStringAsync(url).Result;
-
-            // Get all json data
-            var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
-
-            // Extract just the first array element (This is the only data we are interested in)
-            var translationItems = jsonData as IEnumerable;
-
-            // Translation Data
-            string translation = "";
-
-            // Loop through the collection extracting the translated objects
-            foreach (object item in translationItems)
-            {
-                // Convert the item array to IEnumerable
-                IEnumerable translationLineObject = item as IEnumerable;
-
-                // Convert the IEnumerable translationLineObject to a IEnumerator
-                if(item != null)
-                {
-                IEnumerator translationLineString = translationLineObject.GetEnumerator();
-
-                    
-                // Get first object in IEnumerator
-                translationLineString.MoveNext();
-
-                // Save its value (translated text)
-                translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
-                }
-            }
-
-            // Remove first blank character
-            if (translation.Length > 1) { translation = translation.Substring(1); };
-
-            // Return translation
-            return translation;
-        }
-
     }
 }
